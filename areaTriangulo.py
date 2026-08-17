d3d7552dab1cc124b7cddf9d87f3141b0cecb423
Vitor Destro <vitordestro08@gmail.com>
2026-08-17 11:25:39 -0300

    Cálculo de área em python

---
+++ areaTriangulo.py
@@ -0,0 +1,6 @@
+base = float(input("Insira o valor da base do triângulo: ")).2f
+altura = float(input("Insira o valor da altura  do triângulo: ")).2f
+
+area = (base * altura) / 2
+
+print(f"Área do Triângulo = {area}")
