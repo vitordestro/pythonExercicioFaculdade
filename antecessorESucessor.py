02b7bd91f9ab66d3465f3beffe3d9b41ed78468c
Vitor Destro <vitordestro08@gmail.com>
2026-08-17 11:24:55 -0300

    Cálculo de antecessor e sucessor de um número
    Fix input type check and improve output formatting.

---
+++ antecessorESucessor.py
@@ -0,0 +1,8 @@
+numero = int(input("Insira um numero: "))
+sucessor = numero + 1
+antecessor = numero - 1
+
+if numero = float:
+    print("Impossível calcular o sucessor e antecessor")
+else:
+    print(f"{antecessor} <-- {numero} --> {sucessor}")
