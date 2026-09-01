2c628b2431edc486b69905eae553bd61edbcc450
Vitor Destro <vitordestro08@gmail.com>
2026-09-01 09:32:09 -0300

    Delete antecessorESucessor.py

--- antecessorESucessor.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("Insira um numero: "))
-sucessor = numero + 1
-antecessor = numero - 1
-
-if numero = float:
-    print("Impossível calcular o sucessor e antecessor")
-else:
-    print(f"{antecessor} <-- {numero} --> {sucessor}")
