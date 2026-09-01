dc60ccd3ba870682b2cc6eece7e24dcd6f2d81bb
Vitor Destro <vitordestro08@gmail.com>
2026-09-01 08:20:28 -0300

    Add rain check program in estaChovendo.py

---
+++ estaChovendo.py
@@ -0,0 +1,8 @@
+print("ESTÁ CHOVENDO!\n")
+
+chovendo = input("\nEstá chovendo agora? (sim/não): ").lower()
+
+if chovendo == "não":
+    print("\nNão está chovendo!")
+else:
+    print("\nEstá chovendo!")
