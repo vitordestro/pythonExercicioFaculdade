1b5d6de4b8f26bdf578fefcc912e4c3c17b49f2e
Vitor Destro <vitordestro08@gmail.com>
2026-09-01 09:36:09 -0300

    Delete identificacaoNumeroMaiorOuMenor.py

--- identificacaoNumeroMaiorOuMenor.py
+++
@@ -1,7 +0,0 @@
-numero1 = int(input("Insira um número --> "))
-numero2 = int(input("Insira um segundo número --> "))
-
-if numero1 > numero2:
-    print(f"{numero1} > {numero2}")
-else:
-    print(f"{numero2} > {numero1}")
