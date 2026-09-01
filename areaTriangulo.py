fa750c3eab441b5f6cd07c7163d0cf21b68497d8
Vitor Destro <vitordestro08@gmail.com>
2026-09-01 09:32:27 -0300

    Delete areaTriangulo.py

--- areaTriangulo.py
+++
@@ -1,6 +0,0 @@
-base = float(input("Insira o valor da base do triângulo: ")).2f
-altura = float(input("Insira o valor da altura  do triângulo: ")).2f
-
-area = (base * altura) / 2
-
-print(f"Área do Triângulo = {area}")
