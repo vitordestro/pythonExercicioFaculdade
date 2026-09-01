6da6d87dcddf41494dcb566fa8cb1ab302e6fa46
Vitor Destro <vitordestro08@gmail.com>
2026-09-01 09:35:44 -0300

    Delete perimetroRetangulo.py

--- perimetroRetangulo.py
+++
@@ -1,5 +0,0 @@
-ladoX = int(input("Insira o valor do lado X: "))
-ladoY = int(input("Insira o valor do lado Y: "))
-
-perimetro = (ladoX * 2) + (ladoY * 2)
-print("Valor do Perímetro = {perimetro}")
