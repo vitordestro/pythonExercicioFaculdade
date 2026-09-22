2fd5844821237585ac731f2edee77e82be49d2d3
Vitor Destro <vitordestro08@gmail.com>
2026-09-22 00:08:11 -0300

    Melhoria na logica do exercicio da aula04

--- Aula04/calculadora.py
+++ Aula04/calculadora.py
@@ -1,18 +1,22 @@
-numero1 = int(input("\nInsira um número -->  "))
-numero2 = int(input("\nInsira um segundo número -->  "))
+numero1 = int(input("\nInsira um número --> "))
+numero2 = int(input("\nInsira um segundo número --> "))
 
 soma = numero1 + numero2
 subtracao = numero1 - numero2
 multiplicacao = numero1 * numero2
 
-perguntaOperacao = str(input("\nQual operação deseja realizar?\nescolha entre(+,-,*,/)  --> ").lower())
+perguntaOperacao = input(
+    "\nQual operação deseja realizar?\n"
+    "Escolha entre (+, -, *, /) --> "
+).lower()
 
 if perguntaOperacao == "+":
     print(soma)
+
 elif perguntaOperacao == "-":
-    subtracao
     print(subtracao)
-elif perguntaOperacao == "multiplicação" or perguntaOperacao == "multiplicacao":
+
+elif perguntaOperacao == "*":
     print(multiplicacao)
 
 elif perguntaOperacao == "/":
@@ -21,5 +25,6 @@ elif perguntaOperacao == "/":
     else:
         divisao = numero1 / numero2
         print(divisao)
+
 else:
-    print("Operação Inexistente")
+    print("Operação inexistente")
