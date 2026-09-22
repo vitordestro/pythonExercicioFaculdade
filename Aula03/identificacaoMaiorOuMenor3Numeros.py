99bfa10ffd98a349fe9e3443fe24440be26ecb22
Vitor Destro <vitordestro08@gmail.com>
2026-09-22 00:01:40 -0300

    Melhoria na logica do exercicio

--- Aula03/identificacaoMaiorOuMenor3Numeros.py
+++ Aula03/identificacaoMaiorOuMenor3Numeros.py
@@ -1,10 +1,12 @@
-numero1 = int(input("Insira um número --> "))
-numero2 = int(input("Insira um segundo número --> "))
-numero3 = int(input("Insira um terceiro número --> "))
+numero1 = float(input("Insira o primeiro número: "))
+numero2 = float(input("Insira o segundo número: "))
+numero3 = float(input("Insira o terceiro número: "))
 
 if numero1 > numero2 and numero1 > numero3:
-    print(f"{numero1} > {numero2} E O {numero3}")
+    print(f"{numero1} é o maior número.")
 elif numero2 > numero1 and numero2 > numero3:
-    print(f"{numero2} > {numero1} E O {numero3}")
+    print(f"{numero2} é o maior número.")
+elif numero3 > numero1 and numero3 > numero2:
+    print(f"{numero3} é o maior número.")
 else:
-    print(f"{numero3} > {numero1} E O {numero2}")
+    print("Existem números iguais entre os maiores.")
