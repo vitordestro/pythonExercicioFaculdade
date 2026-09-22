7e45713262898c82f3d49f603b6bc88c4f2cff93
Vitor Destro <vitordestro08@gmail.com>
2026-09-22 00:02:18 -0300

    Melhoria na logica do exercicio

--- Aula03/identificacaoNumeroMaiorOuMenor.py
+++ Aula03/identificacaoNumeroMaiorOuMenor.py
@@ -1,7 +1,9 @@
-numero1 = int(input("Insira um número --> "))
-numero2 = int(input("Insira um segundo número --> "))
+numero1 = float(input("Insira o primeiro número: "))
+numero2 = float(input("Insira o segundo número: "))
 
 if numero1 > numero2:
-    print(f"{numero1} > {numero2}")
+    print(f"{numero1} é maior que {numero2}.")
+elif numero2 > numero1:
+    print(f"{numero2} é maior que {numero1}.")
 else:
-    print(f"{numero2} > {numero1}")
+    print("Os números são iguais.")
