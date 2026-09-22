17b58e92cea03e19e53b3f830f605c3f7ae07d9d
Vitor Destro <vitordestro08@gmail.com>
2026-09-22 00:05:24 -0300

    Melhoria na logica do exercicio

--- Aula03/podeDirigir.py
+++ Aula03/podeDirigir.py
@@ -1,10 +1,10 @@
 nome = input("Insira seu nome: ")
-idade = float(input("Insira sua idade: "))
-cnh = input("Possui cnh? ").lower()
+idade = int(input("Insira sua idade: "))
+cnh = input("Possui cnh? (sim/nao): ").lower()
 
 if idade >= 18 and cnh == "sim":
     print(f"{nome}, você pode dirigir")
+elif idade >= 18 and cnh == "nao":
+    print(f"{nome}, você não pode dirigir.")
 elif idade < 18:
-    print(f"{nome}, você não pode dirigir")
-elif idade >= 18 and cnh == "nao"():
-    print(f"{nome}, você precisa tirar a cnh para dirigir")
+    print(f"{nome}, você ainda não pode dirigir.")
