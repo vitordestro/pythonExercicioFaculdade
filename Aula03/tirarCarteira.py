43f43b07a202e2f42bda00f89aad21369d143138
Vitor Destro <vitordestro08@gmail.com>
2026-09-22 00:34:02 -0300

    Adição de exercicio para entrega da atividade

---
+++ Aula03/tirarCarteira.py
@@ -0,0 +1,16 @@
+idadep1 = float(input("Digite a idade da primeira pessoa: "))
+idadep2 = float(input("Digite a idade da segunda pessoa: "))
+idadep3 = float(input("Digite a idade da terceira pessoa: "))
+
+if idadep1 >= 18:
+  print("Primeira idade pode votar!")
+else: 
+  print("Primeira idade não pode votar!")
+if idadep2 >= 18:
+  print("Segunda idade pode votar!")
+else: 
+  print("Segunda idade não pode votar!")
+if idadep3 >= 18:
+  print("Terceira idade pode votar!")
+else: 
+  print("Terceira idade não pode votar!")
